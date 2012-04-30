4c31951b5ea7db63f86feea26ff04c4c200416ff
Maple <maplevalley8@gmail.com>
2012-04-30 22:58:32 +0800

    update: add library and update main logic

--- src/main.js
+++ src/main.js
@@ -18,19 +18,53 @@ window.onload = function() {
   var gameStart = function(x, y) {
     if (x == y) {
       console.log('loaded!!!')
-      var opts = {
-        width: width,
-        height: height,
-        container: document.getElementById('container'),
-        loader: resourceLoader,
-      };
-      var game = new Game(opts);
-      game.initScene();
-      game.renderLoop();
     }
     console.log('Resource loading...')
   }
 
-  var resourceLoader = new ResourceLoader();
-  resourceLoader.load(gameStart);
+  function startGame() {
+    console.log(123)
+    var opts = {
+      width: width,
+      height: height,
+      container: document.getElementById('container'),
+      loader: loader,
+    };
+    var game = new Game(opts);
+    game.initScene();
+    game.renderLoop();
+  };
+
+  function showProgress(total, loaded) {
+    console.log(loaded+'/'+total);
+  };
+
+  var loader = new ThreePreloader({
+    images: {
+      bg1: 'images/bg_i_heart_sensei_1280_960.jpg',
+      bg2: 'images/bg_store_1280_960.jpg',
+      ringStart: 'images/ring_start.png',
+      apple1: 'models/apple/apple_skin.jpg',
+      apple2: 'models/apple/apple_stem.jpg',
+      apple3: 'models/apple/apple_half.jpg',
+      banana1: 'models/banana/banana_skin.jpg',
+      banana2: 'models/banana/banana_half.jpg',
+      watermelon1 : 'models/watermelon/watermelon_skin.jpg',
+      watermelon2 : 'models/watermelon/watermelon_half.jpg',
+    },
+    objects: {
+      apple1: 'models/apple/apple_half1.js',
+      apple2: 'models/apple/apple_half2.js',
+      banana1: 'models/banana/banana_half1.js',
+      banana2: 'models/banana/banana_half2.js',
+      watermelon1 : 'models/watermelon/watermelon_half1.js',
+      watermelon2 : 'models/watermelon/watermelon_half2.js',
+    },
+    sounds: {},
+    onSuccess: startGame,
+    onProgress: showProgress,
+  });
+  loader.load();
+  //var resourceLoader = new ResourceLoader();
+  //resourceLoader.load(gameStart);
 };
